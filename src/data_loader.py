a113bc98b3175f8b8a7a9add264626a4c2da7a33
Szymon Płotka <plotkaszymon@gmail.com>
2021-07-31 12:30:24 +0200

    Adam to SGD

--- src/data_loader.py
+++ src/data_loader.py
@@ -42,10 +42,6 @@ class FetoscopyDatasetTrain(Dataset):
         image = Image.open(self.images[x])
         mask = Image.open(self.masks[x])
         resize_transform = transforms.Resize(size=(self.x_img_size, self.y_img_size))
-        normalize_transform = transforms.Normalize(
-            mean=[0.485, 0.456, 0.406],
-            std=[0.229, 0.224, 0.225]
-        )
         image = resize_transform(image)
         mask = resize_transform(mask)
 
@@ -101,7 +97,6 @@ class FetoscopyDatasetTrain(Dataset):
         masks = np.array(masks)
 
         image = F.to_tensor(image)
-        image = normalize_transform(image)
         mask = F.to_tensor(masks)
 
         return image, mask
